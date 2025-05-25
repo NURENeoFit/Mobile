fd8a773cf95a79ed61994175d6d1cf38b479653f
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-05-25 14:14:10 +0200

    Added a small structure, screen files: home, login, profile, and common navigation panel

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onTap;
+
+  const CustomBottomNavBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: selectedIndex,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.login),
+          label: 'Login',
+        ),
+      ],
+    );
+  }
+}
