cffff510bd44d7c37c66cb8c0e7de5d8b0f405ed
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-05-26 14:49:56 +0200

    Changed profile screen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,31 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  bool notifications = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center (
-        child: Stack(
-        alignment: Alignment.center,
+      appBar: AppBar(title: const Text('Profile')),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: ListView(
+        padding: const EdgeInsets.all(12),
         children: [
-          Text('👤 Profile', style: TextStyle(fontSize: 24)),
-
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(height: 100),
-              ElevatedButton(
-                onPressed: () => context.go('/login'),
-                child: const Text('Logout'),
-              )
-            ],
-          ),
+          _buildTile(Icons.person, 'My details', onTap: () {}),
+          _buildTile(Icons.star, 'Abonement', onTap: () {}),
+          _buildTile(Icons.stacked_bar_chart, 'Statistics', onTap: () {}),
+          _buildSwitchTile(Icons.notifications, 'Notifications', notifications,
+                  (val) => setState(() => notifications = val) ),
+          _buildTile(Icons.settings, 'Settings', onTap: () {}),
+          _buildTile(Icons.logout, 'Logout', onTap: () { context.go('/login'); }),
         ],
       ),
-      )
+    );
+  }
+
+  Widget _buildTile(IconData icon, String title,
+      {String? subtitle, VoidCallback? onTap}) {
+    return Card(
+      elevation: 1.5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      margin: const EdgeInsets.symmetric(vertical: 6),
+      color: Colors.white,
+      child: ListTile(
+        leading: Icon(icon, color: Colors.deepPurple, size: 24),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+          ),
+        ),
+        subtitle: subtitle != null
+            ? Text(subtitle, style: const TextStyle(color: Colors.grey))
+            : null,
+        trailing: const Icon(Icons.chevron_right, color: Colors.grey),
+        onTap: onTap,
+      ),
+    );
+  }
+
+  Widget _buildSwitchTile(IconData icon, String title, bool value, ValueChanged<bool> onChanged) {
+    return Card(
+      elevation: 1.5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      margin: const EdgeInsets.symmetric(vertical: 6),
+      color: Colors.white,
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+        leading: Icon(icon, color: Colors.deepPurple, size: 24),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+          ),
+        ),
+        trailing: Switch(
+          value: value,
+          onChanged: onChanged,
+          activeColor: Colors.green,
+        ),
+      ),
     );
   }
 }
