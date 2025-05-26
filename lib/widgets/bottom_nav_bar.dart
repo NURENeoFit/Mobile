10db8dbc4f25a87fb6e96645d14feefecfc4ca77
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-05-26 15:22:24 +0200

    Added buttons to navigation bar without screens

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -19,12 +19,17 @@ class CustomBottomNavBar extends StatelessWidget {
         splashColor: Colors.transparent, // disables splash color
       ),
       child: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         currentIndex: selectedIndex,
         onTap: onTap,
+        selectedFontSize: 14,
         selectedItemColor: Colors.deepPurple,
+        unselectedFontSize: 14,
         unselectedItemColor: Colors.grey,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.local_fire_department), label: 'Calories'),
+          BottomNavigationBarItem(icon: Icon(Icons.fitness_center), label: 'Trainings'),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
       ),
