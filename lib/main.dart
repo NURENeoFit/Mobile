a549c0ada266957dfad8260ded9b626936fe2974
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-05-27 14:21:10 +0200

    Added router.dart. Changed main.dart using router.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
-import 'package:neofit_mobile/screens/login/login_screen.dart';
-import 'package:neofit_mobile/screens/register/register_screen.dart';
 import 'package:neofit_mobile/screens/home.dart';
 import 'package:neofit_mobile/screens/calories.dart';
 import 'package:neofit_mobile/screens/trainings.dart';
 import 'package:neofit_mobile/screens/profile.dart';
 
+import 'package:neofit_mobile/router.dart';
 import 'package:neofit_mobile/widgets/bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-final _router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const MainScreen(),
-    ),
-  ],
-);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -39,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       title: 'NeoFit',
       theme: ThemeData(primarySwatch: Colors.blue),
-      routerConfig: _router,
+      routerConfig: appRouter,
     );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,23 @@
+import 'package:go_router/go_router.dart';
+
+import 'package:neofit_mobile/screens/login/login_screen.dart';
+import 'package:neofit_mobile/screens/register/register_screen.dart';
+import 'package:neofit_mobile/main.dart';
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: '/register',
+      builder: (context, state) => const RegisterScreen(),
+    ),
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const MainScreen(),
+    ),
+  ],
+);
