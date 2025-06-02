7ac3e03aa1c3e23baf311d6a5607e9e56f27f3fc
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-06-02 18:13:54 +0200

    Added dependency flutter_riverpod and Changed main.dart in order to deal with this dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:neofit_mobile/design/theme.dart';
 import 'package:neofit_mobile/router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
