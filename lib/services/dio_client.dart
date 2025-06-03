9fae5f6d618c1fbafeb40827210721a10bdcb106
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-06-03 20:37:52 +0200

    Changed AuthInterceptor using AuthStorage (auth_storage.dart)

--- lib/services/dio_client.dart
+++ lib/services/dio_client.dart
@@ -1,37 +1,31 @@
 import 'package:dio/dio.dart';
+import 'package:neofit_mobile/services/auth_storage.dart';
 
 class DioClient {
-  static const String baseUrl = 'http://localhost:5000/api';
-
   static final Dio _dio = Dio(BaseOptions(
-    baseUrl: baseUrl,
+    baseUrl: 'http://localhost:5000/api',
     connectTimeout: const Duration(seconds: 10),
     receiveTimeout: const Duration(seconds: 20),
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
   ))
-  // === Logging all requests and responses ===
     ..interceptors.add(LogInterceptor(
       responseBody: true,
       requestBody: true,
       requestHeader: true,
-    ))
-  // === Custom interceptor for token (example) ===
+    )) //for debugging
     ..interceptors.add(AuthInterceptor());
 
   static Dio get instance => _dio;
 }
 
-// === Example of a custom interceptor for adding a token ===
 class AuthInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    // Here you can dynamically get the token (for example, from SecureStorage/SharedPreferences)
-    // For example, the token is hardcoded:
-    const token = 'YOUR_TOKEN_HERE';
-    if (token.isNotEmpty) {
+    final token = AuthStorage.token;
+    if (token != null && token.isNotEmpty) {
       options.headers['Authorization'] = 'Bearer $token';
     }
     super.onRequest(options, handler);
