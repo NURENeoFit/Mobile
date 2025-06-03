9b172be25e408529f358de858c0f64cade2ef80e
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-06-03 21:51:38 +0200

    Changed the connection line to the test connection json server in dio_client.dart

--- lib/services/dio_client.dart
+++ lib/services/dio_client.dart
@@ -3,7 +3,7 @@ import 'package:neofit_mobile/services/auth_storage.dart';
 
 class DioClient {
   static final Dio _dio = Dio(BaseOptions(
-    baseUrl: 'http://localhost:5000/api',
+    baseUrl: 'http://localhost:5000',
     connectTimeout: const Duration(seconds: 10),
     receiveTimeout: const Duration(seconds: 20),
     headers: {
