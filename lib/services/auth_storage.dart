09f90d152855ef7613bde5f7e8e6e7b7d298d87d
Nikita Horshcharuk <nikita.horshcharuk@nure.ua>
2025-06-03 20:35:54 +0200

    Created auth_storage.dart for AuthToken

---
+++ lib/services/auth_storage.dart
@@ -0,0 +1,29 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthStorage {
+  static const String _tokenKey = 'auth_token';
+  static String? _token;
+
+  /// Load token when app starts
+  static Future<void> loadToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    _token = prefs.getString(_tokenKey);
+  }
+
+  /// Get token from memory
+  static String? get token => _token;
+
+  /// Save the token and update in memory
+  static Future<void> saveToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_tokenKey, token);
+    _token = token;
+  }
+
+  /// Delete the token (for example, when logging out)
+  static Future<void> removeToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_tokenKey);
+    _token = null;
+  }
+}
